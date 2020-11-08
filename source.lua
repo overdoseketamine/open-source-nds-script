f130506b83b9427af7f1b101f298a5c14eb33d5b
elementemerald <22267679+elementemerald@users.noreply.github.com>
2020-11-08 12:02:06 -0700

    added source

---
+++ source.lua
@@ -0,0 +1,172 @@
+--[[
+Open Source NDS Script
+Original by shawnjbragdon#0001
+Modified by elementemerald#4175
+
+https://www.roblox.com/games/189707/Natural-Disaster-Survival
+]]
+
+local broadcastDisaster = false
+local notifyChanges = true
+local autofarm = true
+local prefix = ">";
+
+game:GetService("RunService").Stepped:connect(function()
+    -- this function requires a compatible exploit (Synapse, Sentinel, etc.)
+    setsimulationradius(math.pow(math.huge,math.huge)*math.huge, math.pow(math.huge,math.huge)*math.huge);
+    wait(9e9);
+end);
+
+local ua = {}
+  
+for index, part in pairs(workspace:GetDescendants()) do
+    if part:IsA("Part") and part.Anchored == false and part:IsDescendantOf(game:GetService("Players").LocalPlayer.Character) == false then
+        table.insert(ua,part)
+    end
+end
+  
+for i = 1, #ua do
+    ua[i].Parent = workspace
+    local BD = Instance.new("BodyPosition") -- body position for the parts
+    BD.Parent = ua[i]
+    BD.Name = "BD"
+end
+
+game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+	Text = "{System} Getting things ready, please wait...",
+	Color = Color3.fromRGB(255, 235, 85),
+	Font = Enum.Font.SourceSansBold
+})
+
+if notifyChanges == false then
+	game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+		Text = "{System} You currently have the notifyChanges variable set to \'false\', if you want it on please change the value from \'false\' to \'true\'. You can also use the command \'notifychanges/true\'",
+		Color = Color3.fromRGB(255, 85, 85),
+		Font = Enum.Font.SourceSansBold
+	})
+end
+
+pcall(function()
+	coroutine.resume(coroutine.create(function()
+		SurvivalTag = game:GetService("Players").LocalPlayer.Character:WaitForChild("SurvivalTag")
+		if notifyChanges == true then
+			game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+				Text = string.format("{System} The upcoming disaster will be a %s", SurvivalTag.Value),
+				Color = Color3.fromRGB(255, 235, 85),
+				Font = Enum.Font.SourceSansBold
+			})
+			SurvivalTag:GetPropertyChangedSignal("Value"):Connect(function()
+				game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+					Text = string.format("{System} The upcoming disaster will be a %s", SurvivalTag.Value),
+					Color = Color3.fromRGB(255, 235, 85),
+					Font = Enum.Font.SourceSansBold
+				})
+			end)
+		end
+		if broadcastDisaster == true then
+			game:GetService("ReplicatedStorage")["DefaultChatSystemChatEvents"].SayMessageRequest:FireServer(string.format("[LuminentialBot]: The upcoming disaster will be a %s", SurvivalTag.Value),"All")
+		end
+	end))
+	coroutine.resume(coroutine.create(function()
+		local FallDamageScript = game:GetService("Players").LocalPlayer.Character:WaitForChild("FallDamageScript")
+		FallDamageScript:Destroy()
+		if notifyChanges == true then
+			game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+				Text = "{System} FallDamageScript has been destroyed.",
+				Color = Color3.fromRGB(255, 235, 85),
+				Font = Enum.Font.SourceSansBold
+			})
+		end
+	end))
+end)
+
+game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(Character)
+	coroutine.resume(coroutine.create(function()
+		SurvivalTag = Character:WaitForChild("SurvivalTag")
+		if notifyChanges == true then
+			game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+				Text = string.format("{System} The upcoming disaster will be a %s", SurvivalTag.Value),
+				Color = Color3.fromRGB(255, 235, 85),
+				Font = Enum.Font.SourceSansBold
+			})
+			SurvivalTag:GetPropertyChangedSignal("Value"):Connect(function()
+				game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+					Text = string.format("{System} The upcoming disaster will be a %s", SurvivalTag.Value),
+					Color = Color3.fromRGB(255, 235, 85),
+					Font = Enum.Font.SourceSansBold
+				})
+			end)
+		end
+		if broadcastDisaster == true then
+			game:GetService("ReplicatedStorage")["DefaultChatSystemChatEvents"].SayMessageRequest:FireServer(string.format("[LuminentialBot]: The upcoming disaster will be a %s", SurvivalTag.Value),"All")
+		end
+	end))
+	coroutine.resume(coroutine.create(function()
+		local FallDamageScript = Character:WaitForChild("FallDamageScript")
+		FallDamageScript:Destroy()
+		if notifyChanges == true then
+			game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+				Text = "{System} FallDamageScript has been destroyed.",
+				Color = Color3.fromRGB(255, 235, 85),
+				Font = Enum.Font.SourceSansBold
+			})
+			SurvivalTag:GetPropertyChangedSignal("Value"):Connect(function()
+				game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+					Text = string.format("{System} The upcoming disaster will be a %s", SurvivalTag.Value),
+					Color = Color3.fromRGB(255, 235, 85),
+					Font = Enum.Font.SourceSansBold
+				})
+			end)
+		end
+	end))
+end)
+
+game:GetService("Players").LocalPlayer.Chatted:Connect(function(Message,Recipient)
+	if typeof(Recipient) == "Instance" and table.find({"autofarm","notifychanges","broadcastdisaster"}) then
+		return
+	end
+	local commands = {
+		["autofarm/"] = function(value)
+			local boolean = (string.sub(value,1,4) == "true" and true) or (string.sub(value,1,5) == "false" and false)
+			autofarm = boolean
+		end,
+		["notifychange/"] = function(value)
+			local boolean = (string.sub(value,1,4) == "true" and true) or (string.sub(value,1,5) == "false" and false)
+			notifyChanges = boolean
+		end,
+		["broadcastdisaster/"] = function(value)
+			local boolean = (string.sub(value,1,4) == "true" and true) or (string.sub(value,1,5) == "false" and false)
+			broadcastDisaster = boolean
+		end,
+	};
+	for index,value in pairs(commands) do
+		if string.lower(tostring(index)) == string.sub(string.lower(Message),1,string.len(index)) then
+			commands[index](string.sub(Message,string.len(tostring(index)) + 1))
+
+			game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+				Text = string.format("{System} Ran a command: %s",tostring(index)),
+				Color = Color3.fromRGB(255, 235, 85),
+				Font = Enum.Font.SourceSansBold
+			})
+		end
+	end
+end)
+
+game:GetService("RunService").RenderStepped:Connect(function()
+	pcall(function()
+		if not autofarm then
+			return error("Autofarm is not enabled!")
+		end
+		game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(-280, 180, 385) * CFrame.Angles(math.rad(0), math.rad(0), math.rad(0))
+		game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").RotVelocity = Vector3.new(0, 0, 0)
+		game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart").Velocity = Vector3.new(0, 0, 0)
+	end)
+end)
+
+game:GetService("Players").LocalPlayer.Chatted:connect(function(msg)
+    if msg == prefix .."tpua" then
+        for i = 1, #ua do
+            ua[i].BD.Position = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position;
+        end
+    end
+end);
